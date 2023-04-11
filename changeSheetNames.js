ce86143d36c42c03e8ae0ac26efc1893a772c297
r-goto <g.ryuji0510@gmail.com>
2023-04-11 13:36:23 +0900

    Create changeSheetNames.js
    Changes sheets names of all spreadsheets within those specified folders.

---
+++ changeSheetNames.js
@@ -0,0 +1,23 @@
+//指定されたフォルダ内のすべてワンルーム区域のシート名を一括で変更できます。
+
+function changeSheetNames() {
+  var allArea = { 
+    '高洲': '1_VWlV4zxj6Tt6RLA1cBBhL_2wVE9LVnC',
+    '東野': '1SC0LTvqYQBvyFDMKU5Fwbt5kSwM8gSTj',
+    '冨岡': '1Rjhq6VTvDfFemim5fxn9zwbtx_6X8ayb',
+    '入船': '17-IwLekMA7ay1VlqbZOEHB8XyKfQ9iEn',
+    '今川': '13fJoPCoIYDh-ncY0E8NtUJ9Mr4ZHrfJs'
+  };
+
+  for (var area in allArea) {
+    Logger.log(`すべてワンルーム区域のデータ保護を削除しています。これには時間がかかります。`)
+    var folder = DriveApp.getFolderById(allArea[area]);
+    var files = folder.getFilesByType(MimeType.GOOGLE_SHEETS);
+
+    while (files.hasNext()) {
+      var file = files.next();
+      var sheet = SpreadsheetApp.openById(file.getId()).getActiveSheet();
+      sheet.setName("全部屋番号2");
+    }
+  }
+}
